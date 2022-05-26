2f3e642e03e6cdb4962325cca91e00c0ec1bde9d
TomNewyen <nmnghiep.ityu@gmail.com>
2022-05-26 15:46:43 +0700

    s

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+const Container = styled.div`
+    height: 60px;
+    background-color: black;
+`
+const NavBar = () => {
+  return (
+    <div>
+        NavBarasdasd
+    </div>
+  )
+}
+
+export default NavBar
