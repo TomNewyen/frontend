03a7670108b62085598000b4f59525624f44211c
ITITIU19020 <66402103+ITITIU19020@users.noreply.github.com>
2022-06-08 18:11:19 +0700

    routing in server (for now)

--- server/index.js
+++ server/index.js
@@ -4,9 +4,17 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require("cors");
 
+
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/uers",userRoute);
+app.use("/api/auth",authRoute);
 
 mongoose
   .connect(process.env.MONGO_URL)
