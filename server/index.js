1a29c9f6f91ea5167c3f4bde287f65f312235223
ITITIU19020 <66402103+ITITIU19020@users.noreply.github.com>
2022-06-08 20:09:46 +0700

    Remaing things (product/cart/order/stripe) (no bug *yet)
    Think i got the grip of it

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const cors = require("cors");
 
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 
 dotenv.config();
@@ -15,6 +19,10 @@ app.use(express.json());
 
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 mongoose
   .connect(process.env.MONGO_URL)
