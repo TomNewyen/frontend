e280a524c3c2057832a0b69abfa3cae7a9c20f06
ITITIU19020 <66402103+ITITIU19020@users.noreply.github.com>
2022-06-08 20:11:57 +0700

    Revert "Remaing things (product/cart/order/stripe) (no bug *yet)"
    This reverts commit 1a29c9f6f91ea5167c3f4bde287f65f312235223.

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ const cors = require("cors");
 
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
-const productRoute = require("./routes/product");
-const cartRoute = require("./routes/cart");
-const orderRoute = require("./routes/order");
-const stripeRoute = require("./routes/stripe");
 
 
 dotenv.config();
@@ -19,10 +15,6 @@ app.use(express.json());
 
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
-app.use("/api/products", productRoute);
-app.use("/api/carts", cartRoute);
-app.use("/api/orders", orderRoute);
-app.use("/api/checkout", stripeRoute);
 
 mongoose
   .connect(process.env.MONGO_URL)
