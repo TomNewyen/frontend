cffb7d4391ef978c3ad0255cd9d0823989c059ca
ITITIU19020 <66402103+ITITIU19020@users.noreply.github.com>
2022-06-12 11:51:15 +0700

    add admin

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { store, persistor } from "./redux/store";
-import { PersistGate } from 'redux-persist/integration/react'
+// import { store, persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
+import store from "./redux/store";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
